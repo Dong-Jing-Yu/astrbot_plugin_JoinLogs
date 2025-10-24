4a7a60c9d84fce422093acc0538dbc522524b425
东经雨 <114856851+Dong-Jing-Yu@users.noreply.github.com>
2025-10-25 01:56:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,50 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
+@register("astrbot_plugin_JoinLogs", "东经雨", "记录入群时的一些信息", "1.0")
+class JoinLogsPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
     
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.platform_adapter_type(filter.PlatformAdapterType.AIOCQHTTP)
+    @filter.event_message_type(filter.EventMessageType.GROUP_MESSAGE)
+    async def event_monitoring(self, event: AiocqhttpMessageEvent):
+        """监听进群/退群事件"""
+        raw = getattr(event.message_obj, "raw_message", None)
+        if not isinstance(raw, dict):
+            return
 
+
+        client = event.bot
+        group_id: int = raw.get("group_id", 0)
+        user_id: int = raw.get("user_id", 0)
+        # 进群申请事件
+        if (
+            self.conf["enable_audit"]
+            and raw.get("post_type") == "request"
+            and raw.get("request_type") == "group"
+            and raw.get("sub_type") == "add"
+        ):
+            comment = raw.get("comment")
+            flag = raw.get("flag", "")
+            nickname = (await client.get_stranger_info(user_id=user_id))[
+                "nickname"
+            ] or "未知昵称"
+            reply = f"[进群申请]\n昵称：{nickname}\nQQ：{user_id}\nflag：{flag}"
+            if comment:
+                reply += f"\n{comment}"
+            if self.conf["admin_audit"]:
+                await self._send_admin(client, reply)
+            else:
+                await event.send(event.plain_result(reply))
+
+
+    @filter.command("查",alias={'查入群'， '查进群'})
+    async def Cha(self, event: AstrMessageEvent):
+        pass
+        
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
